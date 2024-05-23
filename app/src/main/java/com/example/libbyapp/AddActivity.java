ae28881728439a039c36c7dfd4518aa2a7ee0836
kaimdnve <kaimdnve@gmail.com>
2024-05-23 16:40:56 +0800

    add

--- app/src/main/java/com/example/libbyapp/AddActivity.java
+++ app/src/main/java/com/example/libbyapp/AddActivity.java
@@ -1,11 +1,11 @@
 package com.example.libbyapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class AddActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/libbyapp/MyDatabaseHelper.java
+++ app/src/main/java/com/example/libbyapp/MyDatabaseHelper.java
@@ -29,46 +29,45 @@ class MyDatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         String query = "CREATE TABLE " + TABLE_NAME +
-                " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                COLUMN_TITLE + " TEXT, " +
-                COLUMN_AUTHOR + " TEXT, " +
-                COLUMN_PAGES + " INTEGER);";
+                        " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        COLUMN_TITLE + " TEXT, " +
+                        COLUMN_AUTHOR + " TEXT, " +
+                        COLUMN_PAGES + " INTEGER);";
         db.execSQL(query);
     }
-
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
     }
 
-    void addBook(String title, String author, int pages) {
+    void addBook(String title, String author, int pages){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues cv = new ContentValues();
 
         cv.put(COLUMN_TITLE, title);
         cv.put(COLUMN_AUTHOR, author);
         cv.put(COLUMN_PAGES, pages);
-        long result = db.insert(TABLE_NAME, null, cv);
-        if (result == -1) {
+        long result = db.insert(TABLE_NAME,null, cv);
+        if(result == -1){
             Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show();
-        } else {
+        }else {
             Toast.makeText(context, "Added Successfully!", Toast.LENGTH_SHORT).show();
         }
     }
 
-    Cursor readAllData() {
+    Cursor readAllData(){
         String query = "SELECT * FROM " + TABLE_NAME;
         SQLiteDatabase db = this.getReadableDatabase();
 
         Cursor cursor = null;
-        if (db != null) {
+        if(db != null){
             cursor = db.rawQuery(query, null);
         }
         return cursor;
     }
 
-    void updateData(String row_id, String title, String author, String pages) {
+    void updateData(String row_id, String title, String author, String pages){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues cv = new ContentValues();
         cv.put(COLUMN_TITLE, title);
@@ -76,25 +75,25 @@ class MyDatabaseHelper extends SQLiteOpenHelper {
         cv.put(COLUMN_PAGES, pages);
 
         long result = db.update(TABLE_NAME, cv, "_id=?", new String[]{row_id});
-        if (result == -1) {
+        if(result == -1){
             Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show();
-        } else {
+        }else {
             Toast.makeText(context, "Updated Successfully!", Toast.LENGTH_SHORT).show();
         }
 
     }
 
-    void deleteOneRow(String row_id) {
+    void deleteOneRow(String row_id){
         SQLiteDatabase db = this.getWritableDatabase();
         long result = db.delete(TABLE_NAME, "_id=?", new String[]{row_id});
-        if (result == -1) {
+        if(result == -1){
             Toast.makeText(context, "Failed to Delete.", Toast.LENGTH_SHORT).show();
-        } else {
+        }else{
             Toast.makeText(context, "Successfully Deleted.", Toast.LENGTH_SHORT).show();
         }
     }
 
-    void deleteAllData() {
+    void deleteAllData(){
         SQLiteDatabase db = this.getWritableDatabase();
         db.execSQL("DELETE FROM " + TABLE_NAME);
     }
